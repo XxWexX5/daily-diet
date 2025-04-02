a56a2aff9b12fa30ac54b6b22f8260e32ea43ef9
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-04-01 23:12:22 -0300

    [ REFACT ] Adjust text.
    - Adjust text to "Hello World!";

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./styles/global.css";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-blue-500 text-xl font-bold">Hello NativeWind!</Text>
+      <Text className="text-blue-500 text-xl font-bold">Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
