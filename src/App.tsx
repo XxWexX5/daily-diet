9073d462f330604ecd68e314405edb0be0b9e516
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-04-02 21:31:43 -0300

    [ FEATURE ] Import svg files.
    - Importing files of types .svg;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+
+import { Text, View } from "react-native";
+
+import Logo from "./assets/images/logo.svg";
 
 import "./styles/global.css";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
+      <View>
+        <Logo width={100} height={100} />
+      </View>
+
       <Text className="text-blue-500 text-xl font-bold">Hello World!</Text>
       <StatusBar style="auto" />
     </View>
