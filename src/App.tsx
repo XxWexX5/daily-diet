bdd631b1fa201d6df51fb3bba042ac672d28fc5e
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-04-07 22:09:16 -0300

    [ FEATURE ] Adding fonts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+
+import { useFonts } from "expo-font";
+
 import { StatusBar } from "expo-status-bar";
 
 import { Text, View } from "react-native";
@@ -6,14 +10,33 @@ import Logo from "@images/logo.svg";
 
 import "@styles/global.css";
 
+import * as SplashScreen from "expo-splash-screen";
+
 export default function App() {
+  const [loaded] = useFonts({
+    Nunito: require("./assets/fonts/NunitoSans/NunitoSans.ttf"),
+    NunitoItalic: require("./assets/fonts/NunitoSans/NunitoSans-Italic.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return <Text>Carregando!</Text>;
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <View>
         <Logo width={100} height={100} />
       </View>
 
-      <Text className="text-blue-500 text-xl font-bold">Hello World!</Text>
+      <Text className="text-blue-500 text-xl font-bold font-nunito">
+        Hello World!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+import { platformSelect } from "nativewind/theme";
+
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        nunitoItalic: ["NunitoItalic", "sans-serif"],
+        system: platformSelect({
+          ios: "Nunito",
+          android: "Nunito",
+          default: "Nunito",
+        }),
+      }
+    }
   },
   plugins: [],
 }
